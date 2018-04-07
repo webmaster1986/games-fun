6bcb267bcf9da5e486ac83c91832a060ca294f94
manoj <manojgodhani00@gmail.com>
2018-04-07 12:54:23 -0700

    change code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,9 +4,7 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="home">
-        <iframe id="fg-frame-solitaire-classic" width="640" height="360" data-aspect-ratio="1.778" src="" frameborder="0"></iframe>
-      </div>
+      <div/>
     )
   }
 }
